95ebe098905270c9e5eaf380e3a3ea040adb0e1a
GKleim <geoff.kleimeyer@gmail.com>
2015-10-08 21:23:32 -0500

    Optimize and clean up code. No major changes.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,118 +1,121 @@
+var bio = {
+  "name": "Geoff Kleimeyer",
+  "role": "Chemical Engineer",
+  "contacts": {
+    "mobile": "513-509-4300",
+    "email": "geoff.kleimeyer@gmail.com",
+    "github": "GKleim",
+    "twitter": "@GKleim",
+    "location": "Houston, TX"
+  },
+  "bioPic": "images/me.jpg",
+  "welcomeMsg": "Welcome",
+  "skills" : [
+    "math",
+    "computer programming",
+    "juggling",
+    "delivering things"
+  ]
+}
+
 var work = {
   "jobs": [
-      {
-            "employer": "Dow Chemical",
-            "title": "Process Engineer",
-            "location": "Houston, TX",
-            "dates": "2014 - Present",
-            "description": "Design chemical processes. This includes designing hydraulic systems, heat transfer systems, and associated safety systems."
-        },
-        {
-            "employer": "The Ohio State University",
-            "title": "General Chemistry Lab Teaching Assistant",
-            "location": "Columbus, OH",
-            "dates": "2011 - 2013",
-            "description": "Teach and supervise lab classes. Grade lab reports and tutor students in general chemistry topics"
-        }
-    ]
+    {
+      "employer": "Dow Chemical",
+      "title": "Process Engineer",
+      "location": "Houston, TX",
+      "dates": "2014 - Present",
+      "description": "Design chemical processes. This includes designing hydraulic systems, heat transfer systems, and associated safety systems."
+    },
+    {
+      "employer": "The Ohio State University",
+      "title": "General Chemistry Lab Teaching Assistant",
+      "location": "Columbus, OH",
+      "dates": "2011 - 2013",
+      "description": "Teach and supervise lab classes. Grade lab reports and tutor students in general chemistry topics"
+    }
+  ]
 }
 
 var projects = {
-    "projects": [
-        {
-            "title": "Dow Agrosciences",
-            "dates": "Feb 2014 - Jun 2014",
-            "description": "Hydraulics",
-            "images": [
-                "http://placehold.it/555x300"
-            ]
-        },
-        {
-            "title": "Polyethylene",
-            "dates": "Jun 2014 - Jan 2015",
-            "description": "Instruments, hydraulics, equipment",
-            "images": [
-                "http://placehold.it/555x300"
-            ]
-        },
-        {
-            "title": "PS&F",
-            "dates": "Jan 2015 - Aug 2015",
-            "description": "Utilities, rundown, vacuum, separations, and safety",
-            "images": [
-                 "http://placehold.it/555x300"
-            ]
-        },
-        {
-            "title": "Monomers & Coatings",
-            "dates": "Aug 2015 - present",
-            "description": "Heaters and hydraulics",
-            "images": [
-                 "http://placehold.it/555x300"
-            ]
-        }
-    ]
-}
-
-var bio = {
-    "name": "Geoff Kleimeyer",
-    "role": "Chemical Engineer",
-    "contacts": {
-        "mobile": "513-509-4300",
-        "email": "geoff.kleimeyer@gmail.com",
-        "github": "GKleim",
-        "twitter": "@GKleim",
-        "location": "Houston, TX"
+  "projects": [
+    {
+      "title": "Dow Agrosciences",
+      "dates": "Feb 2014 - Jun 2014",
+      "description": "Hydraulics",
+      "images": [
+        "http://placehold.it/300x200"
+      ]
     },
-    "bioPic": "images/me.jpg",
-    "welcomeMessage": "Welcome",
-    "skills" : [
-        "math",
-        "computer programming",
-        "juggling",
-        "delivering things"
-        ]
+    {
+      "title": "Plastics",
+      "dates": "Jun 2014 - Jan 2015",
+      "description": "Instruments, hydraulics, equipment",
+      "images": [
+        "http://placehold.it/300x200"
+      ]
+    },
+    {
+      "title": "PS&F",
+      "dates": "Jan 2015 - Aug 2015",
+      "description": "Utilities, vacuum, separations, and safety",
+      "images": [
+        "http://placehold.it/300x200"
+      ]
+    },
+    {
+      "title": "Monomers & Coatings",
+      "dates": "Aug 2015 - present",
+      "description": "Heaters and hydraulics",
+      "images": [
+        "http://placehold.it/300x200"
+      ]
+    }
+  ]
 }
 
 var education = {
-    "schools": [
-        {
-            "name": "The Ohio State University",
-            "location": "Columbus, OH",
-            "degree": "BS",
-            "majors": [
-                "Chemical Engineering"
-            ],
-            "minors": [
-                "Economics"
-            ],
-            "dates": 2013,
-            "url": "http://www.osu.edu"
-        },
-        {
-            "name": "Udacity",
-            "location": "Houston, TX",
-            "degree": "Nanodegree",
-            "majors": [
-                "Front End Developer"
-            ],
-            "dates": 2015,
-            "url": "http://www.udacity.com"
-        }
-    ],
-    "onlineCourses": [
-        {
-            "title": "JavaScript Basics",
-            "school": "Udacity",
-            "dates": 2015,
-            "url": "http://www.udacity.com"
-        }
-    ]
+  "schools": [
+    {
+      "name": "The Ohio State University",
+      "location": "Columbus, OH",
+      "degree": "BS",
+      "majors": [
+        "Chemical Engineering"
+      ],
+      "minors": [
+        "Economics"
+      ],
+      "dates": 2013,
+      "url": "http://www.osu.edu"
+    },
+    {
+      "name": "Udacity",
+      "location": "Houston, TX",
+      "degree": "Nanodegree",
+      "majors": [
+        "Front End Developer"
+      ],
+        "dates": 2015,
+        "url": "http://www.udacity.com"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "JavaScript Basics",
+      "school": "Udacity",
+      "dates": 2015,
+      "url": "http://www.udacity.com"
+    }
+  ]
 }
 
 bio.display = function() {
-  $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
-  $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+  var formattedHeaderRole = HTMLheaderRole.replace("%data%", bio.role)
+  $("#header").prepend(formattedHeaderRole);
+
+  var formattedHeaderName = HTMLheaderName.replace("%data%", bio.name)
+  $("#header").prepend(formattedHeaderName);
 
   $("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
   $("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
@@ -120,50 +123,64 @@ bio.display = function() {
   $("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
   $("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
 
-  $("#header").append(HTMLbioPic.replace("%data%", bio.bioPic));
-  $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+  var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+  $("#header").append(formattedBioPic);
+
+  var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+  $("#header").append(formattedWelcomeMsg);
 
   if(bio.skills.length > 0) {
-      $("#header").append(HTMLskillsStart);
-      for(skill in bio.skills) {
-          $("#skills").append(HTMLskills.replace("%data%", bio.skills[skill]))       
-      }
+    $("#header").append(HTMLskillsStart);
+
+    for(skill in bio.skills) {
+      var formattedSkills = HTMLskills.replace("%data%", bio.skills[skill]);
+      $("#skills").append(formattedSkills);
+    }
   }else{
-      console.log("No skills to post");
+    console.log("No skills to post");
   }
 }
 
 work.display = function() {
   if(work.jobs.length > 0) {
-      var formattedEmployer = "";
-      var formattedTitle = "";
-      $("#workExperience").append(HTMLworkStart);
-      for(job in work.jobs) {
-          formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-          formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-          $(".work-entry:last").append(formattedEmployer + formattedTitle);
-          $(".work-entry:last").append(HTMLworkDates.replace("%data%", work.jobs[job].dates));
-          $(".work-entry:last").append(HTMLworkLocation.replace("%data%", work.jobs[job].location));
-          $(".work-entry:last").append(HTMLworkDescription.replace("%data%", work.jobs[job].description));                  
-      }
+    $("#workExperience").append(HTMLworkStart);
+
+    for(job in work.jobs) {
+      var formattedEmployer = formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+      var formattedTitle = formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+      $(".work-entry:last").append(formattedEmployer + formattedTitle);
+
+      var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+      $(".work-entry:last").append(formattedWorkDates);
+
+      var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+      $(".work-entry:last").append(formattedWorkLocation);
+
+      var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+      $(".work-entry:last").append(formattedWorkDescription);
+    }
   }else{
-      console.log("No jobs to post");
+    console.log("No jobs to post");
   }
 }
 
 projects.display = function() {
   for(project in projects.projects) {
     $("#projects").append(HTMLprojectStart);
-    var formattedprojectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-    $(".project-entry:last").append(formattedprojectTitle);
-    var formattedprojectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-    $(".project-entry:last").append(formattedprojectDates);
-    var formattedprojectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-    $(".project-entry:last").append(formattedprojectDescription);
+
+    var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+    $(".project-entry:last").append(formattedProjectTitle);
+
+    var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+    $(".project-entry:last").append(formattedProjectDates);
+
+    var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+    $(".project-entry:last").append(formattedProjectDescription);
+
     if(projects.projects[project].images.length > 0) {
       for(image in projects.projects[project].images) {
-        var formattedprojectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-        $(".project-entry:last").append(formattedprojectImage);
+        var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+        $(".project-entry:last").append(formattedProjectImage);
       }
     }
   }
@@ -172,18 +189,20 @@ projects.display = function() {
 education.display = function() {
   for(school in education.schools) {
     $("#education").append(HTMLschoolStart);
-    var formattedschoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
-    var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-    $(".education-entry:last").append(formattedschoolName + formattedschoolDegree);
-    var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-    $(".education-entry:last").append(formattedschoolDates);
-    var formattedschoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
-    $(".education-entry:last").append(formattedschoolLocation);
+
+    var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+    var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+    $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+
+    var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+    $(".education-entry:last").append(formattedSchoolDates);
+
+    var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+    $(".education-entry:last").append(formattedSchoolLocation);
 
     for(major in education.schools[school].majors) {
-      var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors[major]);
-      $(".education-entry:last").append(formattedschoolMajor);
-      console.log(formattedschoolMajor);
+      var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors[major]);
+      $(".education-entry:last").append(formattedSchoolMajor);
     }
   }
 
@@ -191,13 +210,16 @@ education.display = function() {
 
   for(course in education.onlineCourses) {
     $("#education").append(HTMLschoolStart);
-    var formattedonlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
-    var formattedonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
-    $(".education-entry:last").append(formattedonlineTitle + formattedonlineSchool);
-    var formattedonlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
-    $(".education-entry:last").append(formattedonlineDates);
-    var formattedonlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
-    $(".education-entry:last").append(formattedonlineURL);
+
+    var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+    $(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+
+    var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+    $(".education-entry:last").append(formattedOnlineDates);
+
+    var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+    $(".education-entry:last").append(formattedOnlineURL);
   }
 }
 
@@ -217,5 +239,5 @@ work.display();
 projects.display();
 education.display();
 
-$("#main").append(internationalizeButton)
 $("#mapDiv").append(googleMap);
+$("#main").append(internationalizeButton);
